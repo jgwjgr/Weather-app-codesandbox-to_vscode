f5ad00752a92c552b5e62b8df65eb7cf77530376
jgwjgr <junishict21@gmail.com>
2023-03-16 16:05:39 +0100

    Move codesandbox project to vscode

---
+++ src/index.js
@@ -0,0 +1,47 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+let dateElement = document.querySelector("h3");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+function displayWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("h2").innerHTML =
+    Math.round(response.data.main.temp) + "°C";
+}
+
+function searchCity(city) {
+  let apiKey = "934d63b7a6b8ae0196e30b31c17ed43d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
